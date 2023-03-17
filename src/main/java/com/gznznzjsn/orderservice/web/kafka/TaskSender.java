093d09dfa3df35d63d8407fdc651e64212e0da08
gznznzjsn <arik2046@gmail.com>
2023-03-17 17:14:45 +0300

    Moved event key to xml settings

--- src/main/java/com/gznznzjsn/orderservice/web/kafka/TaskSender.java
+++ src/main/java/com/gznznzjsn/orderservice/web/kafka/TaskSender.java
@@ -1,15 +1,18 @@
 package com.gznznzjsn.orderservice.web.kafka;
 
+import com.gznznzjsn.orderservice.web.kafka.parser.XMLParser;
+import com.jcabi.xml.XML;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderRecord;
 
-@RequiredArgsConstructor
 @Component
+@RequiredArgsConstructor
 public class TaskSender {
 
+    private final XML taskProducerSettings;
     private final KafkaSender<String, Long> sender;
 
     public void send(Long taskId) {
@@ -19,7 +22,7 @@ public class TaskSender {
                                 "tasks",
                                 0,
                                 System.currentTimeMillis(),
-                                "task",
+                                new XMLParser(taskProducerSettings).parse("requiredTaskKey"),
                                 taskId,
                                 null
                         )
