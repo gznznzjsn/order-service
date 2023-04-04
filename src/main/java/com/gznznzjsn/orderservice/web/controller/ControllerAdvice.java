f36a403251c3cfb4b5e66a5fc9b4f59928bd7d12
gznznzjsn <arik2046@gmail.com>
2023-04-04 10:56:26 +0300

    Added logging for ControllerAdvice

--- src/main/java/com/gznznzjsn/orderservice/web/controller/ControllerAdvice.java
+++ src/main/java/com/gznznzjsn/orderservice/web/controller/ControllerAdvice.java
@@ -6,6 +6,7 @@ import com.gznznzjsn.orderservice.domain.exception.NotEnoughResourcesException;
 import com.gznznzjsn.orderservice.domain.exception.ResourceNotFoundException;
 import com.gznznzjsn.orderservice.domain.exception.UniqueResourceException;
 import com.gznznzjsn.orderservice.web.dto.ExceptionDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,6 +18,7 @@ import java.nio.file.AccessDeniedException;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
 
@@ -24,6 +26,8 @@ public class ControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ExceptionDto handleResourceNotFound(
             ResourceNotFoundException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
         return ExceptionDto.builder()
                 .message(e.getMessage())
                 .build();
@@ -33,6 +37,8 @@ public class ControllerAdvice {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ExceptionDto handleNotEnoughResources(
             ResourceNotFoundException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
         return ExceptionDto.builder()
                 .message(e.getMessage())
                 .build();
@@ -42,6 +48,8 @@ public class ControllerAdvice {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ExceptionDto handleUniqueResource(
             UniqueResourceException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
         return ExceptionDto.builder()
                 .message(e.getMessage())
                 .build();
@@ -51,6 +59,8 @@ public class ControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ExceptionDto handleIllegalAction(
             IllegalActionException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
         return ExceptionDto.builder()
                 .message(e.getMessage())
                 .build();
@@ -58,7 +68,9 @@ public class ControllerAdvice {
 
     @ExceptionHandler({AccessDeniedException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ExceptionDto handleAccessDenied() {
+    public ExceptionDto handleAccessDenied(AccessDeniedException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
         return ExceptionDto.builder()
                 .message("Access denied!")
                 .build();
@@ -67,6 +79,8 @@ public class ControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionDto handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
         Map<String, String> otherInfo = e.getBindingResult()
                 .getFieldErrors().stream()
                 .collect(Collectors.toMap(
@@ -84,7 +98,9 @@ public class ControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ExceptionDto handleOther() {
+    public ExceptionDto handleOther(Exception e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
         return ExceptionDto.builder()
                 .message("Please, try later!")
                 .build();
